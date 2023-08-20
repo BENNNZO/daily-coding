41418d199db6aec544c35e786f474bca0d3415df
Benjamin Phillips <benjamingt537@gmail.com>
2023-08-20 12:08:08 -0400

    got main js working

---
+++ 08 - (08-20-2023)/script.js
@@ -0,0 +1,18 @@
+window.addEventListener("input", e => {
+    let index = parseInt(e.target.getAttribute("id").replace("code-input-", ""))
+
+    // lock input boxes to max char length of 1
+    if (e.target.value.length > 1) {
+        e.target.value = e.target.value.charAt(0)
+    }
+
+    // focus next box when typing
+    if (index !== 4 && e.inputType !== "deleteContentBackward") {
+        document.getElementById(`code-input-${index + 1}`).focus()
+    }
+    
+    // focus last box when backspacing
+    if (index !== 1 && e.inputType === "deleteContentBackward") {
+        document.getElementById(`code-input-${index - 1}`).focus()
+    }
+})
