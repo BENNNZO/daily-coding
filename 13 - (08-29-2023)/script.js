f9d818328626d604b094926cb4a814c5d898b136
Benjamin Phillips <benjamingt537@gmail.com>
2023-08-29 15:57:29 -0400

    got dates working just needs styling

--- 13 - (08-29-2023)/script.js
+++ 13 - (08-29-2023)/script.js
@@ -6,12 +6,42 @@ let date = new Date()
 function init() {
     document.getElementById("month").innerText = monthDict[new Date().getMonth()]
     console.log(new Date().getDay())
-    console.log(new Date().getDate() % 7)
+    console.log(new Date().getDate())
     console.log(daysDict[new Date().getDay()])
+
+    let datesArr = []
+
+    for (let i = 0; i < 5; i++) {
+        for (let j = 0; j < 7; j++) {
+            let index = (i * 7) + j
+            if (index - getStartDate() < 0 || index - getStartDate() >= getDaysInMonth()) {
+                datesArr.push("<div></div>")
+            } else {
+                datesArr.push(`
+                    ${index - 1 < new Date().getDate() ? `
+                        <div class="text-center opacity-50">
+                    ` : index - 1 === new Date().getDate() ? `
+                        <div class="text-center bg-blue-400">
+                    ` : `
+                        <div class="text-center text-bold">
+                    `}
+                        ${index - 1}
+                    </div>
+                `)
+            }
+        }
+    }
+
+    // document.getElementById("dates").innerHTML = datesArr
+    document.getElementById("dates").innerHTML = datesArr.join("")
 }
 
-function daysInMonth() {
+function getDaysInMonth() {
     return new Date(new Date().getFullYear(), new Date().getMonth(), 0).getDate()
+}
+
+function getStartDate() {
+    return new Date().getDay() - new Date().getDate() % 7 + 1
 }
 
 init()
